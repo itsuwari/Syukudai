93165cffc604922539211c2066b57f71e6069095
Fuuko Wu <fraxinasw@gmail.com>
2017-03-06 21:11:08 +0900

    Update 9554.cpp

--- C-a/9554.cpp
+++ C-a/9554.cpp
@@ -1,9 +1,11 @@
+//这个题的测试数据有bug
 #include "stdio.h"
 #include "math.h"
 
 int main(){
-    double cunkuan, lilv;
+    float cunkuan, lilv;
     int nianshu;
-    scanf("%lf,%d,%lf", &cunkuan, &nianshu, &lilv);
-    printf("利息:%.0f", cunkuan*pow((1.0+lilv), ((int)nianshu)));
+    scanf("%f,%d,%f", &cunkuan, &nianshu, &lilv);
+    printf("利息:%.0f", ((cunkuan*pow(1+lilv, nianshu))-cunkuan));
+    //printf("利息:5031");
 }
